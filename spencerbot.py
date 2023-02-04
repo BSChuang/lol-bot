b20f89de3065736b1f59c7b38bb4d4f9a6a648da
Chuang, Benjamin <bschuang@gatech.edu>
2023-02-04 01:44:33 -0500

    only look at sion games for recurring calls

--- spencerbot.py
+++ spencerbot.py
@@ -165,11 +165,13 @@ async def check_for_new_game():
 
         source = get_game_json()
 
-        start_time_str = source['data'][0]['created_at']
-        duration = source['data'][0]['game_length_second']
+        last_game = source['data'][0]
+
+        start_time_str = last_game['created_at']
+        duration = last_game['game_length_second']
         end_time = datetime.strptime(start_time_str, "%Y-%m-%dT%H:%M:%S+09:00") - timedelta(hours=14) + timedelta(seconds=duration)
 
-        if (datetime.now() - end_time).seconds < frequency * 1.5:
+        if (datetime.now() - end_time).seconds < frequency * 1.5 and last_game['queue_info']['game_type'] == 'SOLORANKED' and last_game['myData']['champion_id'] == 14:
             await latest_event.message.respond( f'🚨🚨🚨 NEW SION GAME 🚨🚨🚨')
 
             refresh()
