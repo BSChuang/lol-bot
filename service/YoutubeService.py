df135e65683c6c9e6432560722d2342a325629a5
Chuang, Benjamin <bschuang@gatech.edu>
2024-12-05 00:22:27 -0500

    remove temp file if found

--- service/YoutubeService.py
+++ service/YoutubeService.py
@@ -2,20 +2,26 @@ import os
 import subprocess
 
 async def download_youtube_audio(search_query):
+    temp_file = "./temp/temp.mp3"
+
+    if os.path.exists(temp_file):
+        print(f"Deleting existing file: {temp_file}")
+        os.remove(temp_file)
+
     try:
         # Search and download using yt-dlp
         print(f"Searching for: {search_query}")
         command = [
             "yt-dlp",
             "-x", "--audio-format", "mp3",
-            "--output", "./temp/temp.mp3",  # Save as temp.mp3
+            "--output", temp_file,  # Save as temp.mp3
             f"ytsearch1:{search_query}"
         ]
         subprocess.run(command, check=True)
-        return './temp/temp.mp3'
+        return temp_file
     except Exception as e:
         print(f"An error occurred: {e}")
-        return './temp/temp.mp3'
+        return temp_file
 
 if __name__ == "__main__":
     query = input("Enter YouTube search query: ")
